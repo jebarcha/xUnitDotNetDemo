e221bcee1ce92d3340cfd8938a6f9a443af056df
jebarcha <jebarcha@hotmail.com>
2021-01-29 10:08:58 -0600

    Data driven test - use Theory and InlineData attributes

--- GameEngine.Tests1/PlayerCharacterShould.cs
+++ GameEngine.Tests1/PlayerCharacterShould.cs
@@ -136,5 +136,17 @@ namespace GameEngine.Tests1
         {
             Assert.PropertyChanged(_sut, "Health", () => _sut.TakeDamage(10));
         }
+        
+        [Theory]
+        [InlineData(0, 100)]
+        [InlineData(1, 99)]
+        [InlineData(50, 50)]
+        [InlineData(101, 1)]
+        public void TakeDamage(int damage, int expectedHealth) 
+        {
+            _sut.TakeDamage(damage);
+
+            Assert.Equal(expectedHealth, _sut.Health);
+        }
     }
 }
